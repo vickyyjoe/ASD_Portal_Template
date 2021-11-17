a79dd0fc2002d77bb7b0c3da112d339a9bac71a6
vickyyjoe <66718939+vickyyjoe@users.noreply.github.com>
2021-11-17 21:06:09 +0700

    push

---
+++ asd_portal/pages/OurTeam.js
@@ -0,0 +1,378 @@
+import { Component } from "react";
+import Link from "next/link";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+var bnr3 = "/images/banner/bnr3.jpg";
+export class OurTeam extends Component {
+  render() {
+    return (
+      <>
+        <Header></Header>
+        <div class="page-content bg-white">
+          <div
+            class="dlab-bnr-inr overlay-black-middle text-center bg-pt"
+            style={{ backgroundImage: "url(" + bnr3 + ")" }}
+          >
+            <div class="container">
+              <div class="dlab-bnr-inr-entry align-m text-center">
+                <h1 class="text-white">Meet Our Team</h1>
+                <div class="breadcrumb-row">
+                  <ul class="list-inline">
+                    <li>
+                      <a href="/">Home</a>
+                    </li>
+                    <li>OurTeam</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="section-full content-inner">
+            <div class="container">
+              <div class="section-head text-black text-center">
+                <h2 class="title">Head...</h2>
+              </div>
+              <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="section-full content-inner">
+            <div class="container">
+              <div class="section-head text-black text-center">
+                <h2 class="title">Intern</h2>
+              </div>
+              <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                  <div class="dlab-box m-b30 dlab-team1">
+                    <div class="dlab-media">
+                      <img
+                        width="358"
+                        height="460"
+                        alt=""
+                        src={"/images/our-team/pic1.jpg"}
+                      />
+                    </div>
+                    <div class="dlab-info">
+                      <h4 class="dlab-title">Nashid Martines</h4>
+                      <span class="dlab-position">Director</span>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer></Footer>
+      </>
+    );
+  }
+}
+
+export default OurTeam;
