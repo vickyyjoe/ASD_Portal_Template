7570416318493aa7bfe4fe453ef0918f3b8f15c3
Bellmiro Nathen <bellmirocech@yahoo.co.id>
2021-11-19 16:16:08 +0700

    profileDetails2 done tapi kurang responsive

--- asd_portal/pages/ProfileDetails2.js
+++ asd_portal/pages/ProfileDetails2.js
@@ -3,6 +3,9 @@ import Header from "./components/Header";
 import { Component } from "react";
 import { Progress } from "reactstrap";
 
+import Testmonial10 from "./components/element/Testimonial";
+
+const work1 = "/images/our-work/pic1.jpg";
 export class ProfileDetails2 extends Component {
   render() {
     return (
@@ -92,25 +95,261 @@ export class ProfileDetails2 extends Component {
             </div>
           </div>
 
-          <div className="container clearfix boxshadow m-b30">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-2">
-                  <h4 class="name1">Status</h4>
-                </div>
-                <div className="col-lg-10">
-                  <p class="name1">Available</p>
+          <div className="container clearfix paddingnull">
+            <div
+              className="service-box style3 wow fadeInUp"
+              data-wow-duration="2s"
+              data-wow-delay="0.2s"
+            >
+              <div class="icon-bx-wraper" data-name="01">
+                <div className="icon-content">
+                  <div className="row m-b30">
+                    <div className="col-lg-2">
+                      <h4 className="dlab-tilte">Status</h4>
+                    </div>
+                    <div className="col-lg-10">
+                      <p>Available</p>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-3">
+                      <h5 className="dlab-tilte">Game Web Design</h5>
+                    </div>
+                    <div className="col-lg-3">
+                      <p>Developer</p>
+                    </div>
+                    <div className="col-lg-6">
+                      <h5 className="dlab-tilte">
+                        September 2021 - Desember 2021
+                      </h5>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="row">
-                <div className="col-lg-3">
-                  <h5>Game Web Design</h5>
+            </div>
+          </div>
+
+          <div className="container clearfix paddingnull">
+            <div
+              className="service-box style3 wow fadeInUp"
+              data-wow-duration="2s"
+              data-wow-delay="0.2s"
+            >
+              <div class="icon-bx-wraper" data-name="02">
+                <div className="icon-content">
+                  <div className="row m-b30">
+                    <div className="col">
+                      <h4 className="dlab-tilte">Experience</h4>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-lg-2">
+                      <p>2020 - present</p>
+                    </div>
+                    <div className="col-lg-10">
+                      <h5 className="dlab-tilte">Developer</h5>
+                      <p>
+                        Mendevelop website yang dibutuhkan sesuai kebutuhan para
+                        user Mendevelop website yang dibutuhkan sesuai kebutuhan
+                        para user Mendevelop website yang dibutuhkan sesuai
+                        kebutuhan para user Mendevelop website yang dibutuhkan
+                        sesuai kebutuhan para user Mendevelop website yang
+                        dibutuhkan sesuai kebutuhan para user Mendevelop website
+                        yang dibutuhkan sesuai kebutuhan para user
+                      </p>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-lg-3">
-                  <p>Developer</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="container clearfix paddingnull">
+            <div className="row">
+              <div className="col">
+                <div
+                  className="service-box style3 wow fadeInUp"
+                  data-wow-duration="2s"
+                  data-wow-delay="0.2s"
+                >
+                  <div class="icon-bx-wraper" data-name="03">
+                    <div className="icon-content">
+                      <div className="row m-b30">
+                        <div className="col ">
+                          <h4 className="dlab-tilte ">Skills</h4>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-lg-4">
+                          <div class="icon-bx-wraper1 center">
+                            <div class="icon-bx-sm radius bg-primary m-b20">
+                              <a href="/" class="icon-cell">
+                                <i class="ti-ruler-pencil"></i>
+                              </a>
+                            </div>
+                            <div class="icon-content">
+                              <p className="pcolor">Lorem ipsum dolor</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-lg-4">
+                          <div class="icon-bx-wraper1 center">
+                            <div class="icon-bx-sm radius bg-primary m-b20">
+                              <a href="/" class="icon-cell">
+                                <i class="ti-ruler-pencil"></i>
+                              </a>
+                            </div>
+                            <div class="icon-content">
+                              <p className="pcolor">Lorem ipsum dolor</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-lg-4">
+                          <div class="icon-bx-wraper1 center">
+                            <div class="icon-bx-sm radius bg-primary m-b20">
+                              <a href="/" class="icon-cell">
+                                <i class="ti-ruler-pencil"></i>
+                              </a>
+                            </div>
+                            <div class="icon-content">
+                              <p className="pcolor">Lorem ipsum dolor</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <div className="col-lg-6">
-                  <h5>September 2021 - Desember 2021</h5>
+              </div>
+              <div className="col">
+                <div
+                  className="service-box style3 wow fadeInUp"
+                  data-wow-duration="2s"
+                  data-wow-delay="0.2s"
+                >
+                  <div class="icon-bx-wraper" data-name="04">
+                    <div className="icon-content">
+                      <div className="row m-b30">
+                        <div className="col">
+                          <h4 className="dlab-tilte">Experience</h4>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-lg-4">
+                          <div class="icon-bx-wraper1 center">
+                            <div class="icon-bx-sm radius bg-primary m-b20">
+                              <a href="/" class="icon-cell">
+                                <i class="ti-ruler-pencil"></i>
+                              </a>
+                            </div>
+                            <div class="icon-content">
+                              <p className="pcolor">Lorem ipsum dolor</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-lg-4">
+                          <div class="icon-bx-wraper1 center">
+                            <div class="icon-bx-sm radius bg-primary m-b20">
+                              <a href="/" class="icon-cell">
+                                <i class="ti-ruler-pencil"></i>
+                              </a>
+                            </div>
+                            <div class="icon-content">
+                              <p className="pcolor">Lorem ipsum dolor</p>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="col-lg-4">
+                          <div class="icon-bx-wraper1 center">
+                            <div class="icon-bx-sm radius bg-primary m-b20">
+                              <a href="/" class="icon-cell">
+                                <i class="ti-ruler-pencil"></i>
+                              </a>
+                            </div>
+                            <div class="icon-content">
+                              <p className="pcolor">Lorem ipsum dolor</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="container clearfix paddingnull">
+            <div
+              className="service-box style3 wow fadeInUp"
+              data-wow-duration="2s"
+              data-wow-delay="0.2s"
+            >
+              <div class="icon-bx-wraper" data-name="05">
+                <div className="icon-content">
+                  <div className="row m-b30">
+                    <div className="col">
+                      <h4 className="dlab-tilte">Projects History</h4>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-11 m-l30">
+                      <ul class="list-num-count red list-box">
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Aorem ipsum dolor sit amet</li>
+                        <li>Borem ipsum dolor sit amet</li>
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Aorem ipsum dolor sit amet</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="container clearfix paddingnull">
+            <div
+              className="service-box style3 wow fadeInUp"
+              data-wow-duration="2s"
+              data-wow-delay="0.2s"
+            >
+              <div class="icon-bx-wraper" data-name="06">
+                <div className="icon-content">
+                  <div className="row m-b30">
+                    <div className="col">
+                      <h4 className="dlab-tilte">Appreciation</h4>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-11 m-l30">
+                      <ul class="list-num-count red list-box">
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Aorem ipsum dolor sit amet</li>
+                        <li>Borem ipsum dolor sit amet</li>
+                        <li>Lorem ipsum dolor sit amet</li>
+                        <li>Aorem ipsum dolor sit amet</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="section-full bg-gray content-inner-2">
+            <div class="container">
+              <div class="section-content">
+                <Testmonial10></Testmonial10>
+                <div class="row m-t10 m-l5">
+                  <div class="col-lg-12 midaligner ">
+                    <button
+                      class="site-button green m-r15 radius-xl box-shadow "
+                      type="button"
+                    >
+                      View All
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
