3c040b47f927e0ee11d922ae1cbe7664bd3b093a
vicky joe <vickyyjoe12@gmail.com>
2021-12-10 12:26:53 +0700

    Push Loading Screen

--- asd_portal/pages/_app.js
+++ asd_portal/pages/_app.js
@@ -1,8 +1,56 @@
 import "../styles/globals.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <>
+//       <Component {...pageProps} />
+//     </>
+//   );
+// }
+
+import Router from "next/router";
+import React, { useEffect, useState } from "react";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const start = () => {
+      console.log("start");
+      setLoading(true);
+    };
+    const end = () => {
+      console.log("findished");
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", start);
+    Router.events.on("routeChangeComplete", end);
+    Router.events.on("routeChangeError", end);
+    return () => {
+      Router.events.off("routeChangeStart", start);
+      Router.events.off("routeChangeComplete", end);
+      Router.events.off("routeChangeError", end);
+    };
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <div class="content">
+          <div id="load">
+            <div>G</div>
+            <div>N</div>
+            <div>I</div>
+            <div>D</div>
+            <div>A</div>
+            <div>O</div>
+            <div>L</div>
+          </div>
+        </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 }
 
 export default MyApp;
